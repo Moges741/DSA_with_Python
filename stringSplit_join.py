a7ab51f613113bae0c00401710d41c6aa6e88ded
Moges Sisay <mogesse741@gmail.com>
2025-07-05 20:16:58 +0300

    Update stringSplit_join.py

--- stringSplit_join.py
+++ stringSplit_join.py
@@ -1,9 +1,13 @@
+
 def split_and_join(line):
-    # write your code here
-    x = line.split(" ")
-    return "-".join(x)
+     # write your code here
+     line = line.split()
+     line = "-".join(line)
+     return line
 if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
     print(result)
-    
+
+
+
