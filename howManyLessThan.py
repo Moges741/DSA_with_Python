04324a8a3f48292a03beaa86cfa158768e08ec3a
Moges Sisay <mogesse741@gmail.com>
2025-08-05 15:18:46 +0300

    Create howManyLessThan.py

---
+++ howManyLessThan.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        result = []
+        n = len(nums)
+        
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if nums[j] < nums[i] and j != i:
+                    count += 1
+            result.append(count)
+        
+        return result
