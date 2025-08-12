cd47da66689692240d90122375fe4bb70a419546
Moges Sisay <mogesse741@gmail.com>
2025-08-12 23:10:40 +0300

    Create maxNumberOfCoins.py

---
+++ maxNumberOfCoins.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort(reverse=True)
+        res = 0
+        i = 1
+        n = len(piles)
+        while i < n:
+            res += piles[i]
+            i += 2
+            n -= 1
+        return res
